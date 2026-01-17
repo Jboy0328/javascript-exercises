0ea178ca80cb87a1dfb9120899baf01c50f70987
Jboy0328 <jimmy.esag@gmail.com>
2026-01-16 19:08:46 -0700

    Advance from 01/16/26, completed until exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, timesRepeted) {
+    if (timesRepeted < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < timesRepeted; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
